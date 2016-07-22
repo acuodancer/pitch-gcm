012aa17c44ea83fb24ce03f80672b811c5678f3f
acuodancer <leminhduc74@gmail.com>
2016-07-22 16:45:06 +0800

    Modify User Model to accept private_room_id and pending_request_id

--- app/src/main/java/sg/bb8/pitch/model/User.java
+++ app/src/main/java/sg/bb8/pitch/model/User.java
@@ -6,15 +6,21 @@ import java.io.Serializable;
  * Created by Lincoln on 07/01/16.
  */
 public class User implements Serializable {
-    String id, name, email;
+    String id;
+    String name;
+    String email;
+    private String private_room_id;
+    private String pending_request_id;
 
     public User() {
     }
 
-    public User(String id, String name, String email) {
+    public User(String id, String name, String email, String private_room_id, String pending_request_id) {
         this.id = id;
         this.name = name;
         this.email = email;
+        this.setPrivate_room_id(private_room_id);
+        this.setPending_request_id(pending_request_id);
     }
 
     public String getId() {
@@ -40,4 +46,21 @@ public class User implements Serializable {
     public void setEmail(String email) {
         this.email = email;
     }
+
+    public String getPrivate_room_id() {
+        return private_room_id;
+    }
+
+    public void setPrivate_room_id(String private_room_id) {
+        this.private_room_id = private_room_id;
+    }
+
+    public String getPending_request_id() {
+        return pending_request_id;
+    }
+
+    public void setPending_request_id(String pending_request_id) {
+        this.pending_request_id = pending_request_id;
+    }
+
 }
